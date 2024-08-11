409aaa11dc2686be3f28ed01c6468a71163dce3c
gooflee <gooflee@yahoo.com>
2024-08-11 14:35:15 -0400

    Revert "adding another name"
    This reverts commit f76d39b2ef8f94b32d22a6a1942cb46028450b1c.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,7 +1,6 @@
 # Shipping Cost Calculator
 
-# Here is a new update by goofleeg
-# Here another update by gooflee
+# Here is a new update by goofleegit
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
